1293c1c57f4da71d7d623d8ae1eebb08a7b07f79
Finn <145468206+BHAVIC-FinnJones23@users.noreply.github.com>
2023-11-06 09:48:42 +0000

    Update utilities.h
    added extern for global variables, remember to define in main before use

--- utilities.h
+++ utilities.h
@@ -1,4 +1,13 @@
 #ifndef KASHYYYK6_UTILITIES_H
 #define KASHYYYK6_UTILITIES_H
 
+//day of week, 7pm/9pm, Endoor/Naboo/Tatooine
+extern char dinnerTables[7][2][3];
+//room, {bookingData}
+extern char bookings[6][9];
+
+int checkIn();
+int checkOut();
+int bookTable();
+
 #endif //KASHYYYK6_UTILITIES_H
